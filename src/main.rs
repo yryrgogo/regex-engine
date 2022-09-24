23a33b53bf5224100104485d5edb2f952f9b1b41
yryrgogo <yryrgogo@gmail.com>
2022-09-24 10:47:54 +0900

    :+1: add method to render nfa graph by graphviz dot file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 use compiler::{lexer::Lexer, parser::Parser};
 
-use crate::{
-    automaton::runtime::Runtime,
-    viz::graph_viz::{render_to, Edges},
-};
-
-use std::fs::File;
+use crate::automaton::runtime::Runtime;
 
 mod automaton;
 mod compiler;
-mod viz;
 
 fn main() {
     let mut lexer = Lexer::new("a|(bc)*".to_string());
@@ -19,11 +13,8 @@ fn main() {
     let mut parser = Parser::new(tokens);
     let nfa = parser.parse();
 
+    nfa.render_nfa_graph("nfa.dot");
     println!("{:#?}", nfa);
-
-    let mut f = File::create("example1.dot").unwrap();
-    let edges = Edges(vec![(0, 1), (0, 2), (1, 3), (2, 3), (3, 4), (4, 4)]);
-    render_to(edges, &mut f);
 
     let dfa = nfa.nfa2dfa();
     let mut runtime = Runtime::new(&dfa);
